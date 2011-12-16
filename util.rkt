f322fd528ac06c83b7642acf94b670b55413aebb
Tero Hasu <tero.hasu@hut.fi>
2011-12-16 13:01:37 +0100

    some of my usual utils

---
+++ util.rkt
@@ -0,0 +1,65 @@
+#lang racket
+
+(provide define* define-syntax*)
+
+(define-syntax define*
+  (syntax-rules ()
+    ((_ (name arg ... . rest) body ...)
+     (begin
+       (define (name arg ... . rest) body ...)
+       (provide name)))
+    ((_ (name arg ...) body ...)
+     (begin
+       (define (name arg ...) body ...)
+       (provide name)))
+    ((_ name body ...)
+     (begin
+       (define name body ...)
+       (provide name)))))
+
+(define-syntax define-syntax*
+  (syntax-rules ()
+    ((_ (name stx) body ...)
+     (begin
+       (define-syntax (name stx) body ...)
+       (provide name)))
+    ((_ name body ...)
+     (begin
+       (define-syntax name body ...)
+       (provide name)))))
+
+(define* write-nl
+  (case-lambda
+    ((datum) (begin (write datum) (newline)))
+    ((datum out) (begin (write datum out) (newline out)))))
+
+(define* display-nl
+  (case-lambda
+    ((datum) (begin (display datum) (newline)))
+    ((datum out) (begin (display datum out) (newline out)))))
+
+(define* pretty-nl
+  (case-lambda
+    ((datum) (begin (pretty-print datum) (newline)))
+    ((datum out) (begin (pretty-print datum out) (newline out)))))
+
+(define-syntax* data-for
+  (syntax-rules ()
+    ((_ nm ())
+     (void))
+    ((_ nm ((ctor fld ...) more ...))
+     (begin
+       (struct ctor nm (fld ...) #:transparent)
+       (data-for nm (more ...))))))
+
+(define-syntax* data
+  (syntax-rules ()
+    ((_ nm lst)
+     (begin
+       (struct nm () #:transparent)
+       (data-for nm lst)))))
+
+(define-syntax* fix
+  (syntax-rules ()
+    ((_ fn arg ...)
+     (lambda rest (apply fn arg ... rest)))))
