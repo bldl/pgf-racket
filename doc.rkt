5d7ba3600c8ef1614a81276dfec36be51c0643ce
Tero Hasu <tero@ii.uib.no>
2013-08-13 14:33:58 +0200

    have Concat contain a list of documents rather than just two
    normalization fixes

--- doc.rkt
+++ doc.rkt
@@ -18,26 +18,48 @@ afterwards, important as it may be.
 
 (define-doc* Text s)
 (define-doc* Line)
-(define-doc* Concat d1 d2)
+(define-doc* Concat ds)
 (define-doc* Group d)
+
+(define* (cat . ds)
+  (Concat ds))
 
 (define* (doc->tseq d)
   (match d
     ((Text "") (void))
     ((Text s) (yield (TE #f s)))
     ((Line) (yield (LE #f)))
-    ((Concat d1 d2) (begin (doc->tseq d1) (doc->tseq d2)))
-    ((Group d0) (begin 
-		  (yield (GBeg #f))
-		  (let ((d (normalize d0)))
-		    (when d
-		      (doc->tseq d)))
-		  (yield (GEnd #f))))
+    ((Concat ds) (for-each doc->tseq ds))
+    ((Group d0) (let ((d (normalize d0)))
+		  (when d
+		    (yield (GBeg #f))
+		    (doc->tseq d)
+		    (yield (GEnd #f)))))
     (_ (error "unknown document element" d))))
 
 (define (normalize d)
   (match d
     ((Group d0) (normalize d0))
     ((Text "") #f)
-    ((Concat (Text "") d0) (normalize d0))
+    ((Concat (list)) #f)
+    ((Concat (list-rest (Text "") lst)) (normalize (Concat lst)))
     (_ d)))
+
+(module* main #f
+  (for-each
+   (lambda (d)
+     (pretty-println d)
+     ((producer-compose
+       writeln
+       doc->tseq
+       (thunk (yield d)))))
+   (list
+    (cat
+     (Text "Hello")
+     (Text " ")
+     (Text "world"))
+    (Group (Concat '()))
+    (Group (Concat (list (Text "") (Text ""))))
+    (Group (Group (Text "x")))
+    (Group (cat (Group (Text "a")) (Text "b")))
+    )))
